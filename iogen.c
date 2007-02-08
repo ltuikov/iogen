a5d39c981919ba43685d2bea7a6bd8c27d51c9f5
Luben Tuikov <ltuikov@yahoo.com>
2007-02-07 16:32:38 -0800

    Beginnings of threads
    Signed-off-by: Luben Tuikov <ltuikov@yahoo.com>

--- iogen.c
+++ iogen.c
@@ -1,9 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "clparse.h"
 
+typedef enum { READ, WRITE, RW } rw_t;
+
+struct thread_info {
+	pid_t	pid;
+	int	index;
+
+	unsigned int seed;
+	int	log_only;
+	unsigned long	min_io;
+	unsigned long	max_io;
+	unsigned long	min_span;
+	unsigned long	max_span;
+	rw_t	rw;
+
+	char *device;
+};
+
 static struct prog_opts {
+	int	seed_set;
 	unsigned int	seed;
 	int	log_only;
 	int	num_threads;
@@ -11,6 +34,7 @@ static struct prog_opts {
 	unsigned long	max_io;
 	unsigned long	min_span;
 	unsigned long	max_span;
+	rw_t	rw;
 	int	num_devices;
 	char	**devices;
 } prog_opts;
@@ -55,6 +79,9 @@ int get_seed(char *value, void *_opts)
 		fprintf(stderr, "Incorrect seed: %s\n", value);
 		return -1;
 	}
+
+	opts->seed_set = 1;
+
 	return 0;
 }
 
@@ -155,6 +182,29 @@ int get_devices(int index_last, int argc, char *argv[])
 	return 0;
 }
 
+void free_devices(void)
+{
+	free(prog_opts.devices);
+}
+
+int get_rw_op(char *value, void *_opts)
+{
+	struct prog_opts *opts = _opts;
+
+	if (strcmp(value, "READ") == 0)
+		opts->rw = READ;
+	else if (strcmp(value, "WRITE") == 0)
+		opts->rw = WRITE;
+	else if (strcmp(value, "RW") == 0)
+		opts->rw = RW;
+	else {
+		fprintf(stderr, "Incorrect value for rw: %s\n", value);
+		return -1;
+	}
+
+	return 0;
+}
+
 const struct clparse_opt cmd_opts[] = {
 	{ '\0', "seed", 1, get_seed, "Initial random seed" },
 	{ '\0', "log-only", 0, set_log_only, "Generate logs only" },
@@ -163,14 +213,18 @@ const struct clparse_opt cmd_opts[] = {
 	{ '\0', "max-io", 1, get_max_io, "Maximum IO size" },
 	{ '\0', "max-span", 1, get_max_span, "Maximum span" },
 	{ '\0', "min-span", 1, get_min_span, "Minimum span" },
+	{ '\0', "rw", 1, get_rw_op, "One of: READ, WRITE, RW" },
 };
 
 #define NUM_OPTIONS	(sizeof(cmd_opts)/sizeof(cmd_opts[0]))
 
 int main(int argc, char *argv[])
 {
-	int res;
+	int res, i;
 	int index_last = -1;
+	struct thread_info *thread;
+	char parent_name[255];
+	FILE *fp;
 
 	res = cl_get_prog_opts(argc, argv, cmd_opts, NUM_OPTIONS, &prog_opts,
 			       &index_last, 0);
@@ -188,8 +242,47 @@ int main(int argc, char *argv[])
 	if (res)
 		exit(1);
 
-	for (res = 0; res < prog_opts.num_devices; res++)
-		printf("Device%d: %s\n", res, prog_opts.devices[res]);
+	thread = malloc(prog_opts.num_threads * sizeof(*thread));
+	if (!thread) {
+		fprintf(stderr, "Out of memory\n");
+		exit(1);
+	}
+
+	if (!prog_opts.seed_set)
+		prog_opts.seed = rand();
+	srand(prog_opts.seed);
+
+	sprintf(parent_name, "/tmp/iogen.%d", getpid());
+	fp = fopen(parent_name, "w+");
+	if (fp == NULL) {
+		fprintf(stderr, "Couldn't open %s: %s\n",
+			parent_name, strerror(errno));
+		exit(1);
+	}
+
+	for (i = 0; i < prog_opts.num_threads; i++) {
+		pid_t pid;
+
+		thread[i].index = i;
+		thread[i].seed = rand();
+		thread[i].log_only = prog_opts.log_only;
+		thread[i].min_io = prog_opts.min_io;
+		thread[i].max_io = prog_opts.max_io;
+		thread[i].min_span = prog_opts.min_span;
+		thread[i].max_span = prog_opts.max_span;
+		thread[i].rw = prog_opts.rw;
+		thread[i].device = prog_opts.devices[i%prog_opts.num_devices];
+
+		if ((pid = fork()) == 0) {
+			/* child, never returns */
+			do_thread(&thread[i]);
+		} else {
+			thread[i].pid = pid;
+		}
+	}
+
+	free(thread);
+	free_devices();
 
 	return 0;
 }
